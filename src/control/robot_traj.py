0d4912d140e470235d69fecc27ad3e297ca3def1
Kwogger <kwogger@gmail.com>
2010-11-15 02:03:58 -0500

    Moved drawing of robot function to the amr.draw module

--- src/control/robot_traj.py
+++ src/control/robot_traj.py
@@ -8,81 +8,7 @@ Created on Nov 14, 2010
 import math
 import numpy as np
 from matplotlib import pyplot
-
-
-def drawcar(x, y, h=0, scale=1, fig=0):
-  '''Plots a car at position x,y heading h and size scale on figure number fig.
-
-  The default x,y = (0,0), h = 0 points to the right, and scale=1 plots a car
-  with a body of radius 2 units.
-
-  Args:
-    x: The x-coordinate to draw the car
-    y: The y-coordinate to draw the car
-    h: The heading of the car
-    scale: The scale to draw the car
-    fig: The number of the figure to draw the car in.
-  '''
-  # Make a circle for the body
-  t = np.mat(np.arange(0, 2 * math.pi, 0.01))
-  length = len(t.T)
-  bx = np.sin(t)
-  by = np.cos(t)
-
-  # Wheel locations on body
-  wh1 = round(length / 4) - 1
-  wh2 = round(3 * length / 4) - 1
-
-  # Draw the wheels
-  wwidth = 0.2
-  wheight = 0.4
-  w = np.mat([[0, -wheight],
-              [wwidth, -wheight],
-              [ wwidth, wheight],
-              [ 0, wheight],
-              [ 0, 0]])
-
-  # Body top
-  top = round(length / 2)
-  # Top pointer
-  pwidth = 0.1
-  pheight = 0.2
-  tp = np.mat([[pwidth / 2, 0],
-               [ 0, -pheight],
-               [ -pwidth / 2, 0],
-               [ pwidth / 2, 0]])
-
-  # Car outline
-  car = np.bmat([[bx[0, :wh1].T, by[0, :wh1].T],
-                [bx[0, wh1] + w[:, 0], by[0, wh1] + w[:, 1]],
-                [bx[0, wh1:wh2].T, by[0, wh1:wh2].T],
-                [bx[0, wh2] - w[:, 0], by[0, wh2] - w[:, 1]],
-                [bx[0, wh2:].T, by[0, wh2:].T]])
-
-  point = np.bmat([bx[0, top] + tp[:, 0], by[0, top] + tp[:, 1]])
-
-  # Size scaling
-  car = scale * car
-  point = scale * point
-
-  # Rotation matrix
-  R = np.mat([[np.cos(h + math.pi / 2), -np.sin(h + math.pi / 2)],
-              [np.sin(h + math.pi / 2), np.cos(h + math.pi / 2)]])
-  car = (R * car.T).T
-  point = (R * point.T).T
-
-  # Centre
-  car[:, 0] = car[:, 0] + x
-  car[:, 1] = car[:, 1] + y
-  point[:, 0] = point[:, 0] + x
-  point[:, 1] = point[:, 1] + y
-
-  # Plot
-  pyplot.figure(fig)
-  pyplot.plot(car[:, 0], car[:, 1], 'b')
-  pyplot.plot(car[:, 0], car[:, 1], 'b')
-  pyplot.plot(point[:, 0], point[:, 1], 'r')
-  pyplot.axis('equal')
+from amr import draw
 
 
 if __name__ == '__main__':
@@ -114,7 +40,7 @@ if __name__ == '__main__':
   pyplot.hold(True)
   pyplot.plot(xd[0, :].T, xd[1, :].T)
   for t in xrange(0, len(T), 3):
-    drawcar(xd[0, t], xd[1, t], xd[2, t], .05, 2)
+    draw.drawcar(xd[0, t], xd[1, t], xd[2, t], .05, 2)
   pyplot.title('Desired Trajectory');
   pyplot.axis('equal')
 
@@ -134,7 +60,7 @@ if __name__ == '__main__':
   pyplot.hold(True)
   pyplot.plot(xd[0, :].T, xd[1, :].T)
   for t in xrange(0, len(T), 3):
-    drawcar(xd[0, t], xd[1, t], xd[2, t], .2, 3)
+    draw.drawcar(xd[0, t], xd[1, t], xd[2, t], .2, 3)
   pyplot.title('Desired Trajectory');
   pyplot.axis('equal')
 
@@ -169,7 +95,7 @@ if __name__ == '__main__':
     pyplot.hold(True)
     pyplot.plot(xd[0, :].T, xd[1, :].T)
 #    for t in xrange(0, len(T), 5):
-#      drawcar(xd[0, t], xd[1, t], xd[2, t], .3, 5)
+#      draw.drawcar(xd[0, t], xd[1, t], xd[2, t], .3, 5)
     pyplot.title('Desired Trajectory')
     pyplot.axis('equal')
 
