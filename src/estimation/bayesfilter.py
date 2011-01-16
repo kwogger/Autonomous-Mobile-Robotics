264f4d6ec0c9b080b5d01c7009a8938fc8dbe2bd
kwogger <Michael Kwan@Odin>
2011-01-15 22:33:46 -0500

    Modified Bayes filter example

--- src/estimation/bayesfilter.py
+++ src/estimation/bayesfilter.py
@@ -29,9 +29,14 @@ if __name__ == '__main__':
   pos = np.array([5, 5])
   x[pos[0] + n * (pos[1] - 1)] = 1
   # Define an input vector
-  u = np.zeros((1, T))
+  u = np.zeros(T)
   # Define a measurement vector
   y = np.zeros((N, T))
+  xp = np.zeros((2, T))
+  yp = np.zeros((2, T))
+  xt = np.zeros(T)
+  yt = np.zeros(T)
+  y_bel = np.zeros(T)
 
 
   ## Create the motion model, 
@@ -41,7 +46,7 @@ if __name__ == '__main__':
   mot_mod = np.zeros((N, N, 4))
   for i in xrange(n):
     for j in xrange(n):
-      cur = i + j * n;
+      cur = i + j * n
       # Move up
       if j > 0:
         mot_mod[cur - n, cur, 0] = 0.8
@@ -49,19 +54,19 @@ if __name__ == '__main__':
       else:
         mot_mod[cur, cur, 0] = 1
       # Move right
-      if i < n:
+      if i < n - 1:
         mot_mod[cur + 1, cur, 1] = 0.8
         mot_mod[cur, cur, 1] = 0.2
       else:
         mot_mod[cur, cur, 1] = 1
       # Move down
-      if j < n:
+      if j < n - 1:
         mot_mod[cur + n, cur, 2] = 0.8
         mot_mod[cur, cur, 2] = 0.2
       else:
         mot_mod[cur, cur, 2] = 1
       # Move left
-      if i > 1:
+      if i > 0:
         mot_mod[cur - 1, cur, 3] = 0.8
         mot_mod[cur, cur, 3] = 0.2
       else:
@@ -81,36 +86,36 @@ if __name__ == '__main__':
   for i in xrange(1, n - 1):
     for j in xrange(1, n - 1):
       cur = i + (j - 1) * n
-      meas_mod[cur - n + np.arange(-1, 1, 1), cur] = meas_mod_rel[0, :]
-      meas_mod[cur + np.arange(-1, 1, 1), cur] = meas_mod_rel[1, :]
-      meas_mod[cur + n + np.arange(-1, 1, 1), cur] = meas_mod_rel[2, :]
+      meas_mod[cur - n + np.arange(-1, 2), cur] = meas_mod_rel[0, :]
+      meas_mod[cur + np.arange(-1, 2), cur] = meas_mod_rel[1, :]
+      meas_mod[cur + n + np.arange(-1, 2), cur] = meas_mod_rel[2, :]
 
   # Fill in boundaries by dropping impossible measurements
   scale = 1 - sum(meas_mod_rel[0, :])
   for i in xrange(1, n - 1):
       #Top
       cur = i
-      meas_mod[cur + np.arange(-1, 1, 1), cur] = meas_mod_rel[1, :] / scale
-      meas_mod[cur + n + np.arange(-1, 1, 1), cur] = meas_mod_rel[2, :] / scale
+      meas_mod[cur + np.arange(-1, 2), cur] = meas_mod_rel[1, :] / scale
+      meas_mod[cur + n + np.arange(-1, 2), cur] = meas_mod_rel[2, :] / scale
       #Right
       cur = i * n
-      meas_mod[cur - 1 + n * np.arange(-1, 1, 1), cur] = meas_mod_rel[:, 0] / scale
-      meas_mod[cur + n * np.arange(-1, 1, 1), cur] = meas_mod_rel[:, 1] / scale
+      meas_mod[cur - 1 + n * np.arange(-1, 2), cur] = meas_mod_rel[:, 0] / scale
+      meas_mod[cur + n * np.arange(-1, 2), cur] = meas_mod_rel[:, 1] / scale
       #Bottom
       cur = (n - 1) * n + i
-      meas_mod[cur - n + np.arange(-1, 1, 1), cur] = meas_mod_rel[0, :] / scale
-      meas_mod[cur + np.arange(-1, 1, 1), cur] = meas_mod_rel[1, :] / scale
+      meas_mod[cur - n + np.arange(-1, 2), cur] = meas_mod_rel[0, :] / scale
+      meas_mod[cur + np.arange(-1, 2), cur] = meas_mod_rel[1, :] / scale
       #Left
       cur = (i - 1) * n + 1
-      meas_mod[cur + n * np.arange(-1, 1, 1), cur] = meas_mod_rel[:, 1] / scale
-      meas_mod[cur + 1 + n * np.arange(-1, 1, 1), cur] = meas_mod_rel[:, 2] / scale
+      meas_mod[cur + n * np.arange(-1, 2), cur] = meas_mod_rel[:, 1] / scale
+      meas_mod[cur + 1 + n * np.arange(-1, 2), cur] = meas_mod_rel[:, 2] / scale
 
 
-  # Fill in corners, assume fixed 
-  meas_mod[[1, 2, n + 1, n + 2], 1] = .25
-  meas_mod[[n - 1, n, 2 * n, -1, 2 * n], n] = .25
-  meas_mod[[n * (n - 2) + 1, n * (n - 2) + 2, n * (n - 1) + 1, n * (n - 1) + 2], n * (n - 1) + 1] = .25
-  meas_mod[[n * (n - 1) - 1, n * (n - 1), n * n - 1, n * n], n * n] = .25
+  # Fill in corners, assume fixed
+  meas_mod[[0, 1, n, n + 1], 0] = .25
+  meas_mod[[n - 2, n - 1, 2 * n - 2, 2 * n - 1], n - 1] = .25
+  meas_mod[[n * (n - 2), n * (n - 2) + 1, n * (n - 1), n * (n - 1) + 1], n * (n - 1)] = .25
+  meas_mod[[n * (n - 1) - 2, n * (n - 1) - 1, n * n - 2, n * n - 1], n * n - 1] = .25
 
   ## Define an initial belief for the vehicle states
   # 0 = no obstacle, 1 = obstacle
@@ -120,7 +125,9 @@ if __name__ == '__main__':
   pyplot.figure(1)
   pyplot.clf()
   pyplot.hold(True)
-  beliefs = np.reshape(bel, n, n)
+
+  beliefs = np.copy(bel)
+  beliefs.shape = (n, n)
   pyplot.imshow(beliefs, cm.get_cmap('summer'))
   pyplot.plot(pos[1], pos[0], 'ro', markersize=6, linewidth=2)
   pyplot.title('True state and beliefs')
@@ -130,22 +137,23 @@ if __name__ == '__main__':
   for t in xrange(T):
     ## Simulation
     # Select motion input
-    u[t] = np.ceil(4 * np.random.rand(1))
+    u[t] = np.ceil(4 * np.random.rand(1)) - 1
     # Select a motion
     thresh = np.random.rand(1)
-    new_x = np.find(np.cumsum(np.squeeze(mot_mod[:, :, u(t)]) * x[:, t]) > thresh, 1);
+    new_x = np.argwhere(np.cumsum(np.squeeze(mot_mod[:, :, u[t]]) * x[:, t]) > thresh)[0]
     # Move vehicle
     x[new_x, t + 1] = 1
     # Take measurement
-    thresh = np.random.rand(1)
-    new_y = find(np.cumsum(meas_mod[:, :]*x[:, t + 1]) > thresh, 1)
+    thresh = np.random.rand(1) -1
+    new_y = np.argwhere(np.cumsum(meas_mod[:, :]*x[:, t + 1]) > thresh)[0]
     y[new_y, t] = 1
     # Store for plotting
-    curx = np.reshape(x[:, t + 1].T, n, n)
-    [xi, xj] = find(curx > 0);
+    curx = x[:, t + 1].T
+    curx.shape = (n, n)
+    [xi, xj] = np.argwhere(curx > 0);
     xp[:, t] = np.array([[xi], [xj]])
-    cury = bp.reshape(y[:, t].T, n, n)
-    [yi, yj] = find(cury > 0);
+    cury = np.reshape(y[:, t].T, n, n)
+    [yi, yj] = np.argwhere(cury > 0);
     yp[:, t] = np.array([[yi], [yj]])
     xt[t] = new_x;
     yt[t] = new_y;
@@ -155,7 +163,7 @@ if __name__ == '__main__':
     belp = np.squeeze(mot_mod[:, :, u[t]]) * bel
     # Measurement update
     bel = meas_mod[new_y, :].T * belp
-    bel = bel / norm(bel)
+    bel = bel / np.linalg.norm(bel)
 
     [pmax, y_bel[t]] = max(bel)
 
@@ -165,7 +173,7 @@ if __name__ == '__main__':
     pyplot.plot(xj, xi, 'ro', markersize=6, linewidth=2)
     pyplot.plot(yj, yi, 'bx', markersize=4, linewidth=1)
     pyplot.title('True state and beliefs')
-    pause(0.1)
+    #pause(0.1)
 #    F(t+1) = getframe
 
   pyplot.figure(2)
